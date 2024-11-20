6acf0a9d9ea5fbd65bf18cd753db04d9a3986ef4
azamatxabibullayev <azamatxabibullayev02@gmail.com>
2024-11-20 12:06:18 +0500

    3rd

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, Link, Resume, Skill, About
 
-# Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Link)
+admin.site.register(Resume)
+admin.site.register(Skill)
+admin.site.register(About)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Project, Link, Resume, Skill, About
 
-# Create your views here.
+
+def landing_page(request):
+    projects = Project.objects.all()
+    links = Link.objects.all()
+    resume = Resume.objects.last()
+    skills = Skill.objects.all()
+    about = About.objects.last()
+
+    context = {
+        'projects': projects,
+        'links': links,
+        'resume': resume,
+        'skills': skills,
+        'about': about,
+    }
+    return render(request, 'landing_page.html', context)
